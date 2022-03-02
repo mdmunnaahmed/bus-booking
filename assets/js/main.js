98a444495e30a8ae7b2dde16576741e82de5c396
mu2na <msdmunna77@gmail.com>
2022-03-02 12:57:00 +0600

    past version

---
+++ assets/js/main.js
@@ -0,0 +1,209 @@
+'user strict';
+
+// Preloader
+$(window).on('load', function () {
+    $('.preloader').fadeOut(1000);
+});
+
+//Menu Dropdown
+$("ul>li>.sub-menu").parent("li").addClass("has-sub-menu");
+
+$('.menu li a').on('click', function (e) {
+  var element = $(this).parent('li');
+  if (element.hasClass('open')) {
+    element.removeClass('open');
+    element.find('li').removeClass('open');
+    element.find('ul').slideUp(400, "swing");
+  } else {
+    element.addClass('open');
+    element.children('ul').slideDown(400, "swing");
+    element.siblings('li').children('ul').slideUp(400, "swing");
+    element.siblings('li').removeClass('open');
+    element.siblings('li').find('li').removeClass('open');
+    element.siblings('li').find('ul').slideUp(400, "swing");
+  }
+});
+
+// Responsive Menu
+var headerTrigger = $('.header-trigger');
+headerTrigger.on('click', function(){
+    $('.menu,.header-trigger').toggleClass('active')
+    $('.overlay').toggleClass('overlay-color')
+    $('.overlay').removeClass('active')
+});
+
+var headerTrigger2 = $('.top-bar-trigger');
+headerTrigger2.on('click', function(){
+    $('.header-top').toggleClass('active')
+    $('.overlay').toggleClass('overlay-color')
+    $('.overlay').removeClass('active')
+});
+
+var over = $('.overlay');
+over.on('click', function() {
+  $('.overlay').removeClass('overlay-color')
+  $('.menu, .header-trigger').removeClass('active')
+  $('.header-top').removeClass('active')
+})
+
+
+// Sticky Menu
+window.addEventListener('scroll', function(){
+  var header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 0);
+
+});
+var prevScrollpos = window.pageYOffset;
+var scrollPosition = window.scrollY;
+if (scrollPosition >= 1) {
+  $(".header").addClass('active');
+}
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    $(".header-atop").addClass('active');
+    $(".header-atop").removeClass('inActive');
+  } else {
+    $(".header-atop").removeClass('active');
+    $(".headera-top").addClass('inActive');
+  }
+  prevScrollpos = currentScrollPos;
+  if (currentScrollPos === 0) {
+    $(".headera-top").removeClass('active');
+  }
+};
+
+// Nice Select
+$('select').niceSelect();
+
+// Scroll To Top 
+var scrollTop = $(".scrollToTop");
+$(window).on('scroll', function () {
+  if ($(this).scrollTop() < 500) {
+    scrollTop.removeClass("active");
+  } else {
+    scrollTop.addClass("active");
+  }
+});
+
+//Click event to scroll to top
+$('.scrollToTop').on('click', function () {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 300);
+  return false;
+});
+
+// pagination
+$('.pagination li a').on('click', function() {
+  $('.pagination li a').removeClass('active')
+  $(this).addClass('active')
+} )
+
+
+   
+// testimonial slider
+$('.testimonial-slider').slick({
+  fade: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  infinite: true,
+  autoplay: true,
+  pauseOnHover: true,
+  centerMode: true,
+  dots: true,
+  arrows: false,
+  // asNavFor: '.testimonial-img-slider',
+  nextArrow: '<i class="las la-arrow-right arrow-right"></i>',
+  prevArrow: '<i class="las la-arrow-left arrow-left"></i> ',
+});
+
+// testimonial slider
+$('.amenities-slider').slick({
+  fade: false,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  infinite: true,
+  autoplay: true,
+  pauseOnHover: true,
+  centerMode: false,
+  dots: false,
+  arrows: false,
+  // asNavFor: '.testimonial-img-slider',
+  nextArrow: '<i class="las la-arrow-right arrow-right"></i>',
+  prevArrow: '<i class="las la-arrow-left arrow-left"></i> ',
+  responsive: [
+    {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+
+  ]
+});
+
+//Faq
+$('.faq-wrapper .faq-title, .faq-wrapper-two .faq-title-two').on('click', function (e) {
+  var element = $(this).parent('.faq-item, .faq-item-two');
+  if (element.hasClass('open')) {
+    element.removeClass('open');
+    element.find('.faq-content, .faq-content-two').removeClass('open');
+    element.find('.faq-content, .faq-content-two').slideUp(300, "swing");
+  } else {
+    element.addClass('open');
+    element.children('.faq-content, .faq-content-two').slideDown(300, "swing");
+    element.siblings('.faq-item, .faq-item-two').children('.faq-content, .faq-content-two').slideUp(300, "swing");
+    element.siblings('.faq-item, .faq-item-two').removeClass('open');
+    element.siblings('.faq-item, .faq-item-two').find('.faq-title, .faq-title-two').removeClass('open');
+    element.siblings('.faq-item, .faq-item-two').find('.faq-content, .faq-content-two').slideUp(300, "swing");
+  }
+});
+
+// Date Picker
+$('#start-date').datepicker()
+$('#end-date').datepicker()
+$('#start-date2').datepicker()
+$('#end-date2').datepicker()
+
+// Bus Ticket Filter 
+$('.ticket-type li a').on('click', function() {
+  $('.ticket-type li a').removeClass('active')
+  $(this).addClass('active')
+})
+
+
+// Select Seats
+$('.seat-wrapper .seat').on('click', function() {
+  $(this).toggleClass('selected')
+})
+// Seat Expand
+$('.select-seat-btn').on('click', function() {
+  $(this).closest('.ticket-item').children('.seat-plan-wrapper').toggleClass('selected');
+})
+// Close Pane
+$('.tab-pane .close-btn').on('click', function() {
+  $(this).parent().removeClass('active');
+})
---
+++ gulpfile.js
@@ -0,0 +1,142 @@
+const gulp = require("gulp");
+const { series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const cssbeautify = require("gulp-cssbeautify");
+const uglify = require("gulp-uglify");
+const uglifycss = require("gulp-uglifycss");
+// const imagemin = require('gulp-imagemin');
+
+function style() {
+    return gulp
+        .src("./assets/scss/main.scss")
+        .pipe(sass())
+        .pipe(cssbeautify())
+        .pipe(gulp.dest("./assets/css/"));
+}
+
+// function imgMini() {
+//   return gulp.src('./assets/images/**/*.*')
+//     .pipe(imagemin([
+//       imagemin.gifsicle({
+//         interlaced: true
+//       }),
+//       imagemin.jpegtran({
+//         progressive: true
+//       }),
+//       imagemin.optipng({
+//         optimizationLevel: 5
+//       }),
+//       imagemin.svgo({
+//         plugins: [{
+//             removeViewBox: true
+//           },
+//           {
+//             cleanupIDs: false
+//           }
+//         ]
+//       })
+//     ]))
+//     .pipe(gulp.dest('./assets/images/'))
+// }
+
+function copyHtml() {
+    return gulp
+        .src("./*.html")
+        .pipe(gulp.dest("./ready-file/demo-file/demo/"))
+        .pipe(gulp.dest("./ready-file/client-file/"));
+}
+function copyContactPhp() {
+    return gulp
+        .src("./*.php")
+        .pipe(gulp.dest("./ready-file/demo-file/demo/"))
+        .pipe(gulp.dest("./ready-file/client-file/"));
+}
+function copySass() {
+    return gulp
+        .src("./assets/scss/**/*.*")
+        .pipe(gulp.dest("./ready-file/client-file/assets/scss/"));
+}
+
+function copyfonts() {
+    return gulp
+        .src("./assets/fonts/*.*")
+        .pipe(gulp.dest("./ready-file/demo-file/demo/assets/fonts/"))
+        .pipe(gulp.dest("./ready-file/client-file/assets/fonts/"));
+}
+
+function copywebfonts() {
+    return gulp
+        .src("./assets/webfonts/*.*")
+        .pipe(gulp.dest("./ready-file/demo-file/demo/assets/webfonts/"))
+        .pipe(gulp.dest("./ready-file/client-file/assets/webfonts/"));
+}
+
+function copyImages() {
+    return gulp
+        .src("./assets/images/**/*.*")
+        .pipe(gulp.dest("./ready-file/demo-file/demo/assets/images/"))
+        .pipe(gulp.dest("./ready-file/client-file/assets/images/"));
+}
+
+function copyJs() {
+    return gulp
+        .src("./assets/js/**/*.*")
+        .pipe(gulp.dest("./ready-file/demo-file/demo/assets/js/"))
+        .pipe(gulp.dest("./ready-file/client-file/assets/js/"));
+}
+
+function miniJs() {
+    return (
+        gulp
+            .src("./assets/js/**/*.*")
+            // .pipe(concat('main.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest("./ready-file/demo-file/demo/assets/js/"))
+    );
+}
+
+function copyCss() {
+    return gulp
+        .src("./assets/css/**/*.js")
+        .pipe(gulp.dest("./ready-file/demo-file/demo/assets/css/"))
+        .pipe(gulp.dest("./ready-file/client-file/assets/css/"));
+}
+
+function miniCss() {
+    return gulp
+        .src("./assets/css/**/*.css")
+        .pipe(uglifycss())
+        .pipe(gulp.dest("./ready-file/demo-file/demo/assets/css/"));
+}
+
+function watch() {
+    gulp.watch("./assets/scss/**/*.scss", style);
+}
+
+exports.default = series(
+    style,
+    copyHtml,
+    copyfonts,
+    copywebfonts,
+    copyImages,
+    copyJs,
+    miniJs,
+    copyCss,
+    miniCss,
+    copyContactPhp,
+    copySass
+);
+
+exports.watch = watch;
+exports.style = style;
+exports.copyHtml = copyHtml;
+exports.copyfonts = copyfonts;
+exports.copywebfonts = copywebfonts;
+exports.copyImages = copyImages;
+exports.miniJs = miniJs;
+exports.copyJs = copyJs;
+exports.copyCss = copyCss;
+exports.miniCss = miniCss;
+exports.copyContactPhp = copyContactPhp;
+exports.copySass = copySass;
