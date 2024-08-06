bbc6a19893f540ca9a544434e79d185ccaa19ad5
mdmunnaahmed <msdmunna77@gmail.com>
2024-08-06 14:23:53 +0600

    ready for new start

--- gulpfile.js
+++ gulpfile.js
@@ -1,142 +1,50 @@
 const gulp = require("gulp");
 const { series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-const concat = require("gulp-concat");
-const cssbeautify = require("gulp-cssbeautify");
-const uglify = require("gulp-uglify");
-const uglifycss = require("gulp-uglifycss");
-// const imagemin = require('gulp-imagemin');
+const fileinclude = require("gulp-file-include");
+const browserSync = require("browser-sync").create();
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
 
 function style() {
-    return gulp
-        .src("./assets/scss/main.scss")
-        .pipe(sass())
-        .pipe(cssbeautify())
-        .pipe(gulp.dest("./assets/css/"));
+  return gulp
+    .src("./src/assets/sass/main.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      postcss([
+        autoprefixer({
+          cascade: false,
+        }),
+      ])
+    )
+    .pipe(gulp.dest("./src/assets/css"))
+    .pipe(browserSync.stream());
 }
 
-// function imgMini() {
-//   return gulp.src('./assets/images/**/*.*')
-//     .pipe(imagemin([
-//       imagemin.gifsicle({
-//         interlaced: true
-//       }),
-//       imagemin.jpegtran({
-//         progressive: true
-//       }),
-//       imagemin.optipng({
-//         optimizationLevel: 5
-//       }),
-//       imagemin.svgo({
-//         plugins: [{
-//             removeViewBox: true
-//           },
-//           {
-//             cleanupIDs: false
-//           }
-//         ]
-//       })
-//     ]))
-//     .pipe(gulp.dest('./assets/images/'))
-// }
-
-function copyHtml() {
-    return gulp
-        .src("./*.html")
-        .pipe(gulp.dest("./ready-file/demo-file/demo/"))
-        .pipe(gulp.dest("./ready-file/client-file/"));
-}
-function copyContactPhp() {
-    return gulp
-        .src("./*.php")
-        .pipe(gulp.dest("./ready-file/demo-file/demo/"))
-        .pipe(gulp.dest("./ready-file/client-file/"));
-}
-function copySass() {
-    return gulp
-        .src("./assets/scss/**/*.*")
-        .pipe(gulp.dest("./ready-file/client-file/assets/scss/"));
-}
-
-function copyfonts() {
-    return gulp
-        .src("./assets/fonts/*.*")
-        .pipe(gulp.dest("./ready-file/demo-file/demo/assets/fonts/"))
-        .pipe(gulp.dest("./ready-file/client-file/assets/fonts/"));
-}
-
-function copywebfonts() {
-    return gulp
-        .src("./assets/webfonts/*.*")
-        .pipe(gulp.dest("./ready-file/demo-file/demo/assets/webfonts/"))
-        .pipe(gulp.dest("./ready-file/client-file/assets/webfonts/"));
-}
-
-function copyImages() {
-    return gulp
-        .src("./assets/images/**/*.*")
-        .pipe(gulp.dest("./ready-file/demo-file/demo/assets/images/"))
-        .pipe(gulp.dest("./ready-file/client-file/assets/images/"));
-}
-
-function copyJs() {
-    return gulp
-        .src("./assets/js/**/*.*")
-        .pipe(gulp.dest("./ready-file/demo-file/demo/assets/js/"))
-        .pipe(gulp.dest("./ready-file/client-file/assets/js/"));
-}
-
-function miniJs() {
-    return (
-        gulp
-            .src("./assets/js/**/*.*")
-            // .pipe(concat('main.js'))
-            .pipe(uglify())
-            .pipe(gulp.dest("./ready-file/demo-file/demo/assets/js/"))
-    );
-}
-
-function copyCss() {
-    return gulp
-        .src("./assets/css/**/*.js")
-        .pipe(gulp.dest("./ready-file/demo-file/demo/assets/css/"))
-        .pipe(gulp.dest("./ready-file/client-file/assets/css/"));
-}
-
-function miniCss() {
-    return gulp
-        .src("./assets/css/**/*.css")
-        .pipe(uglifycss())
-        .pipe(gulp.dest("./ready-file/demo-file/demo/assets/css/"));
+function htmlfileinclude() {
+  return gulp
+    .src("./src/pages/*.html")
+    .pipe(
+      fileinclude({
+        prefix: "@@",
+        basepath: "@file",
+      })
+    )
+    .pipe(gulp.dest("./src/"))
+    .pipe(browserSync.stream({ once: true }));
 }
 
 function watch() {
-    gulp.watch("./assets/scss/**/*.scss", style);
+  browserSync.init({
+    server: {
+      baseDir: "./src/",
+    },
+  });
+
+  gulp.watch("./src/pages/*.html", htmlfileinclude);
+  gulp.watch("./src/partial/*.html", htmlfileinclude);
+  gulp.watch("./src/assets/sass/**/*.scss", style);
 }
 
-exports.default = series(
-    style,
-    copyHtml,
-    copyfonts,
-    copywebfonts,
-    copyImages,
-    copyJs,
-    miniJs,
-    copyCss,
-    miniCss,
-    copyContactPhp,
-    copySass
-);
-
 exports.watch = watch;
-exports.style = style;
-exports.copyHtml = copyHtml;
-exports.copyfonts = copyfonts;
-exports.copywebfonts = copywebfonts;
-exports.copyImages = copyImages;
-exports.miniJs = miniJs;
-exports.copyJs = copyJs;
-exports.copyCss = copyCss;
-exports.miniCss = miniCss;
-exports.copyContactPhp = copyContactPhp;
-exports.copySass = copySass;
+exports.build = series(htmlfileinclude, style);
